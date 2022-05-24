397a5856b44d20cd2b58911ba5fb38c2d5300f70
Batuhan Baybaş <b.baybas@gmail.com>
2022-05-25 02:36:12 +0300

    update readme

--- src/Redux/features/userSlice.js
+++ src/Redux/features/userSlice.js
@@ -4,10 +4,16 @@ const userSlice = createSlice({
     name: 'user',
     initialState: {
         user: null
+    },
+    reducers: {
+        setUser: (state, action) => {
+            state.user = action.payload;
+        }
     }
 })
 
 
+export const {setUser} = userSlice.actions;
 export const selectUser = state => state.user.user
 
 export default userSlice.reducer
