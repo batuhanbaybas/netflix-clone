49d9c29a6a7600c570031add61ad0e4bc56c0c86
Batuhan Baybaş <b.baybas@gmail.com>
2022-05-26 01:57:37 +0300

    user exist

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,19 +1,19 @@
-import {useEffect, useState} from "react";
+import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
+import {selectUser} from "../../Redux/features/userSlice";
+import {auth} from "../../Server/firebase";
+import useTransition from "../../Hooks/Transition/useTransition";
 
 const NavBar = () => {
-    const [show, setShow] = useState(false);
+    const user = useSelector(selectUser)
+    const {show} = useTransition(window)
 
-    const transitionNavbar = () => {
-        if (window.scrollY > 100) {
-            setShow(true);
-        } else {
-            setShow(false);
-        }
+
+    const handleLogout = () => {
+        auth.signOut()
     }
-    useEffect(() => {
-        window.addEventListener("scroll", transitionNavbar);
-        return () => window.removeEventListener("scroll", transitionNavbar);
-    }, [])
+
+
     return (
         <nav className={`nav ${show && "nav_black"}`}>
             <div className={"nav_content"}>
@@ -22,10 +22,15 @@ const NavBar = () => {
                     alt={'logo'}
                     className={"nav_logo"}
                 />
-                <img
-                    src={"https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"}
-                    alt={'avatar'}
-                    className={"nav_avatar"}/>
+                {
+                    user ?
+                        <img
+                            onClick={handleLogout}
+                            src={"https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117"}
+                            alt={'avatar'}
+                            className={"nav_avatar"}/> :
+                        <Link className={"login_button"} to={"/login"}>Log In</Link>
+                }
             </div>
 
         </nav>
